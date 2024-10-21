69dcc0b00d4f68c0e495e163dec538155ed8f11c
Prashant <prashant@Prashants-MacBook-Pro.local>
2024-10-21 21:14:15 +0530

    completed

---
+++ src/constants/api_constants.py
@@ -0,0 +1,13 @@
+
+class APIConstants(object):
+    def base_url(self):
+        return "https://restful-booker.herokuapp.com"
+
+    def url_create_booking(self):
+        return "https://restful-booker.herokuapp.com/booking"
+
+    def url_create_tooken(self):
+        return "https://restful-booker.herokuapp.com/auth"
+
+    def url_path_put_delete(booking_id):
+        return "https://restful-booker.herokuapp.com/booking" + str(booking_id)
